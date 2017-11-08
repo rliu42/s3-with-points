72bd3702de7e77ef4f6e7c18f91a62265d0b5709
Runpeng Liu <rliu42@mit.edu>
2017-11-08 12:54:35 -0500

    Improve usage of javascript idioms for module exports

--- logger.js
+++ logger.js
@@ -12,25 +12,35 @@ penaltyWordsRef.on("value", function(ss) {
     penalty_words = ss.val() || [];
 });
 
-var fields_to_log = ["from", "to", "cc", "subject", "messageId"];
-exports.log_mail = function(mail) {
-    mailRef.once("value", function(ss) {
-        var data = ss.val() || [];
-        var currentId = data.length;
-        var record = {};
-        fields_to_log.forEach(function(field) {
-            if (mail[field])
-                record[field] = mail[field];
+
+var Logger = function() {
+    var that = Object.create(Logger.prototype);
+
+    that.log_mail = function(mail) {
+        mailRef.once("value", function(ss) {
+            var data = ss.val() || [];
+            var currentId = data.length;
+            var record = {};
+            fields_to_log.forEach(function(field) {
+                if (mail[field])
+                    record[field] = mail[field];
+            });
+            record.penalty_words = penalty_words.filter(function(word) {
+                return ((mail.text || "") + (mail.subject || "")).toLowerCase().indexOf(word) > -1;
+            });
+            record.penalty = record.penalty_words.length;
+            record.timestamp = new Date(mail.date).getTime();
+            var i = data.findIndex(function(mail) {
+                return mail.messageId === record.messageId;
+            });
+            if (i == -1) mailRef.child(currentId).set(record);
         });
-        record.penalty = penalty_words.filter(function(word) {
-            return ((mail.text || "") + (mail.subject || "")).toLowerCase().indexOf(word) > -1;
-        }).length;
-        record.timestamp = new Date(mail.date).getTime();
-        var i = data.findIndex(function(mail) {
-            return mail.messageId === record.messageId;
-        });
-        if (i == -1) mailRef.child(currentId).set(record);
-    });
+    }
+
+    that.mailRef = mailRef;
+
+    Object.freeze(that);
+    return that;
 }
 
-exports.mailRef = mailRef;
+module.exports = Logger();
